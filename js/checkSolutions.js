f04053015975b9e473cda3f897358b687a059583
yzk-jssc <egonik1337@gmail.com>
2022-09-25 14:23:37 +0300

    code refactoring, handle one more condition
    condition that first time is bigger than second

--- js/checkSolutions.js
+++ js/checkSolutions.js
@@ -1,13 +1,24 @@
 function howLongIsFlying (firstTime,secondTime,thirdNumber) {
   const firstTimeInMinutes = +firstTime.split(':')[0] * 60 + +firstTime.split(':')[1]
   const secondTimeInMinutes = +secondTime.split(':')[0] * 60 + +secondTime.split(':')[1]
-  const hours = Math.floor((secondTimeInMinutes - firstTimeInMinutes) / 60)
-  const minutes = (secondTimeInMinutes - firstTimeInMinutes) - 60 
+  let hours,
+      minutes;
+  if(firstTimeInMinutes < secondTimeInMinutes) {
+    hours = Math.floor((secondTimeInMinutes - firstTimeInMinutes) / 60)
+    minutes = (secondTimeInMinutes - firstTimeInMinutes) - 60
+  }else if(firstTimeInMinutes > secondTimeInMinutes){
+    hours = Math.floor((firstTimeInMinutes - secondTimeInMinutes) / 60)
+    minutes = (firstTimeInMinutes - secondTimeInMinutes) - 60 
+  }else {
+    return '00:00'
+  }
   return hours + ':' + ((minutes).toString().length > 1 ? minutes.toString().split('').at(-2) + minutes.toString().split('').at(-1) : '0' + minutes)
   //c минутами костыль)))
 }
 
-console.log(howLongIsFlying('12:00','13:05','0'))
-console.log(howLongIsFlying('12:00','13:00','0'))
-console.log(howLongIsFlying('00:00','6:25','0'))
-console.log(howLongIsFlying('01:20','12:50','0'))
+console.log(howLongIsFlying('12:00','13:05'))
+console.log(howLongIsFlying('12:00','13:00'))
+console.log(howLongIsFlying('00:00','6:25'))
+console.log(howLongIsFlying('01:20','12:50'))
+console.log(howLongIsFlying('6:25','00:22'))
+
