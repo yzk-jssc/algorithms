37929031f3ca592334964c9d3069a13ed2ca37d4
yzk-jssc <egonik1337@gmail.com>
2022-09-25 14:09:59 +0300

    howLongIsFlying

--- js/Solutions/howLongIsFlying.js
+++ js/Solutions/howLongIsFlying.js
@@ -1,13 +1,13 @@
-function howLongIsFlying (firstTime,secondTime,thirdNumber) {
+function howLongIsFlying (firstTime,secondTime) {
   const firstTimeInMinutes = +firstTime.split(':')[0] * 60 + +firstTime.split(':')[1]
   const secondTimeInMinutes = +secondTime.split(':')[0] * 60 + +secondTime.split(':')[1]
   const hours = Math.floor((secondTimeInMinutes - firstTimeInMinutes) / 60)
   const minutes = (secondTimeInMinutes - firstTimeInMinutes) - 60 
   return hours + ':' + ((minutes).toString().length > 1 ? minutes.toString().split('').at(-2) + minutes.toString().split('').at(-1) : '0' + minutes)
-  //c минутами костыль)))
+  //c минутами костыль =)))
 }
 
-console.log(howLongIsFlying('12:00','13:05','0'))
-console.log(howLongIsFlying('12:00','13:00','0'))
-console.log(howLongIsFlying('00:00','6:25','0'))
-console.log(howLongIsFlying('01:20','12:50','0'))
+console.log(howLongIsFlying('12:00','13:05'))
+console.log(howLongIsFlying('12:00','13:00'))
+console.log(howLongIsFlying('00:00','6:25'))
+console.log(howLongIsFlying('01:20','12:50'))
